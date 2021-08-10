c6f04c67b22da025aeac4acf94700e317d4d543b
Gy <406631892@qq.com>
2021-08-10 16:06:38 +0800

    tree动画

--- src/Tree/TreeItem.tsx
+++ src/Tree/TreeItem.tsx
@@ -1,7 +1,8 @@
-import React, { ChangeEventHandler, useState } from 'react';
+import React, { ChangeEventHandler, useEffect, useRef, useState } from 'react';
 import classnames from 'classnames';
 import { AiFillCaretDown } from 'react-icons/ai';
 import { sourceDataItem, TreeProps } from '@/Tree/Tree';
+import { useDidMountEffect } from '@/hooks/useDidMountEffect';
 
 interface Props {
   item: sourceDataItem;
@@ -10,7 +11,7 @@ interface Props {
 };
 
 const TreeItem: React.FC<Props> = (props) => {
-  const { item, level, treeProps} = props;
+  const { item, level, treeProps } = props;
   const [expended, setExpended] = useState(true);
   const classes = classnames('g-tree-item', {
     [`level-${level}`]: level,
@@ -18,6 +19,37 @@ const TreeItem: React.FC<Props> = (props) => {
   const checked = treeProps.multiple
     ? treeProps.selected.includes(item.value)
     : treeProps.selected === item.value;
+  const divRef = useRef<HTMLDivElement>(null);
+  useDidMountEffect(() => {
+    if (expended) {
+      if (!divRef.current) return;
+      divRef.current.style.height = 'auto';
+      const { height } = divRef.current.getBoundingClientRect();
+      divRef.current.style.height = '0px'
+      divRef.current.getBoundingClientRect();
+      divRef.current.style.height = height + 'px';
+      const afterExpand = () => {
+        if (!divRef.current) {return;}
+        divRef.current.style.height = '';
+        divRef.current.classList.add('present');
+        divRef.current.removeEventListener('transitionend', afterExpand);
+      };
+      divRef.current.addEventListener('transitionend', afterExpand);
+    } else {
+      if (!divRef.current) return;
+      const { height } = divRef.current?.getBoundingClientRect();
+      divRef.current.style.height = height + 'px';
+      divRef.current.getBoundingClientRect();
+      divRef.current.style.height = '0px';
+      const afterCollapse = ()=>{
+        if (!divRef.current) {return;}
+        divRef.current.style.height = '';
+        divRef.current.classList.add('gone');
+        divRef.current.removeEventListener('transitionend', afterCollapse);
+      }
+      divRef.current.addEventListener('transitionend', afterCollapse);
+    }
+  }, [expended]);
   const change: ChangeEventHandler<HTMLInputElement> = (e) => {
     if (treeProps.multiple) {
       if (e.target.checked) {
@@ -27,10 +59,10 @@ const TreeItem: React.FC<Props> = (props) => {
         treeProps.onChange(treeProps.selected.filter((value: string) => value !== item.value));
       }
     } else {
-      if(e.target.checked){
+      if (e.target.checked) {
         treeProps.onChange(item.value);
-      }else{
-        treeProps.onChange('')
+      } else {
+        treeProps.onChange('');
       }
     }
   };
@@ -39,19 +71,19 @@ const TreeItem: React.FC<Props> = (props) => {
   };
   return (
     <div key={item.value} className={classes}>
-      <div className="g-tree-item-text">
+      <div className='g-tree-item-text'>
         <span
           className={classnames('icon', { collapsed: !expended })}
           onClick={handleExpend}
         >
           {item.children && <AiFillCaretDown />}
         </span>
-        <input type="checkbox" checked={checked} onChange={change} />
+        <input type='checkbox' checked={checked} onChange={change} />
         {item.text}
       </div>
-      <div className={classnames('g-tree-children', { collapsed: !expended })}>
-        {item.children?.map((sub,index) => {
-          return <TreeItem item={sub} treeProps={treeProps} level={level + 1} key={index}/>
+      <div ref={divRef} className={classnames('g-tree-children', { collapsed: !expended })}>
+        {item.children?.map((sub, index) => {
+          return <TreeItem item={sub} treeProps={treeProps} level={level + 1} key={index} />;
         })}
       </div>
     </div>

a345b3cc5ac8bbd7d5955a538b7549e400a0bd51
Gy <406631892@qq.com>
2022-03-08 17:32:34 +0800

    点击打开关闭

--- src/Image/Image.tsx
+++ src/Image/Image.tsx
@@ -8,15 +8,17 @@ interface ImageProps extends React.ImgHTMLAttributes<HTMLImageElement> {}
 const Image: React.FC<ImageProps> = (props) => {
   const { className, src, ...rest } = props;
   const classnames = classes('banana-image', className);
-  const [visible, setVisible] = useState<boolean>(true);
+  const [visible, setVisible] = useState<boolean>(false);
+  const showLightUp = () => {
+    setVisible(true);
+  };
+  const hideLightUp = () => {
+    setVisible(false);
+  };
   return (
     <div className={classnames}>
-      <img src={src} {...rest} />
-      <LightUp
-        src={src}
-        visible={visible}
-        setVisible={(visible) => setVisible(!visible)}
-      />
+      <img src={src} onClick={showLightUp} {...rest} />
+      <LightUp src={src} visible={visible} close={hideLightUp} />
     </div>
   );
 };
@@ -24,16 +26,19 @@ const Image: React.FC<ImageProps> = (props) => {
 interface LightUpProps {
   src?: string;
   visible: boolean;
-  setVisible: (visible: boolean) => void;
+  close: (visible: boolean) => void;
 }
 
 const LightUp: React.FC<LightUpProps> = (props) => {
-  const { src, visible, setVisible } = props;
+  const { src, visible, close } = props;
+  const handleClose = () => {
+    close();
+  };
   const render = () => {
     return (
       visible && (
         <div className="banana-image-lightup-wrapper">
-          <div className="banana-image-lightup-mask" />
+          <div className="banana-image-lightup-mask" onClick={handleClose} />
           <div className="banana-image-lightup-content-wrapper">
             <img className="banana-image-lightup-content" src={src} />
           </div>

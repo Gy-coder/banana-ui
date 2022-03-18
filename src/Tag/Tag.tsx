b94cbd6f4d6d3d78261e16e3147b227794b22b8e
Gy <406631892@qq.com>
2022-03-18 15:49:44 +0800

    搭建Tag组件

---
+++ src/Tag/Tag.tsx
@@ -0,0 +1,10 @@
+import React from 'react';
+import './Tag.scss';
+
+export interface TagProps {}
+
+const Tag: React.FC<TagProps> = (props) => {
+  return <div>Tag</div>;
+};
+
+export default Tag;
---
+++ src/Tag/demos/basic.tsx
@@ -0,0 +1,9 @@
+import React from 'react';
+// @ts-ignore
+import { Tag } from 'banana-ui';
+
+const Demo = () => {
+  return <Tag />;
+};
+
+export default Demo;

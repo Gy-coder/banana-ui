b3a3e6988065e384954cb7a238c0dd4dcbff1a4d
Gy <406631892@qq.com>
2022-03-23 20:42:13 +0800

    确定基本的DrawerProps

--- src/Drawer/Drawer.tsx
+++ src/Drawer/Drawer.tsx
@@ -1,10 +1,45 @@
-import React from 'react';
+import React, { ReactNode } from 'react';
 import './Drawer.scss';
 
-export interface DrawerProps {}
+export interface DrawerProps {
+  visible: boolean;
+  onChange: (visible: boolean) => void;
+  closeable?: boolean;
+  container?: HTMLElement | false;
+  placement?: 'top' | 'left' | 'bottom' | 'right';
+  width?: number;
+  zIndex?: number;
+  showMask?: boolean;
+  maskClosable?: boolean;
+  title?: ReactNode;
+}
 
 const Drawer: React.FC<DrawerProps> = (props) => {
+  const {
+    visible,
+    onChange,
+    closeable,
+    placement,
+    container,
+    width,
+    zIndex,
+    showMask,
+    maskClosable,
+    title,
+    children,
+  } = props;
+
   return <div>Drawer</div>;
+};
+
+Drawer.defaultProps = {
+  closeable: true,
+  placement: 'right',
+  width: 384,
+  zIndex: 1000,
+  showMask: true,
+  maskClosable: true,
+  container: document.body,
 };
 
 export default Drawer;

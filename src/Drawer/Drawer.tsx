2e8c5fb1842786f193f68f713209ba685f565016
Gy <406631892@qq.com>
2022-03-24 12:45:14 +0800

    Drawer支持右侧划入

--- src/Drawer/Drawer.tsx
+++ src/Drawer/Drawer.tsx
@@ -1,9 +1,12 @@
-import React, { ReactNode } from 'react';
+import React, { CSSProperties, ReactNode, useEffect, useRef } from 'react';
+import ReactDOM from 'react-dom';
+import classnames from 'classnames';
+import { CloseCircleOutlined } from '@ant-design/icons';
 import './Drawer.scss';
 
 export interface DrawerProps {
   visible: boolean;
-  onChange: (visible: boolean) => void;
+  onClose: () => void;
   closeable?: boolean;
   container?: HTMLElement | false;
   placement?: 'top' | 'left' | 'bottom' | 'right';
@@ -12,24 +15,87 @@ export interface DrawerProps {
   showMask?: boolean;
   maskClosable?: boolean;
   title?: ReactNode;
+  footer?: ReactNode;
+  headerStyle?: CSSProperties;
+  mainStyle?: CSSProperties;
+  footerStyle?: CSSProperties;
 }
 
 const Drawer: React.FC<DrawerProps> = (props) => {
+  const cn = 'g-drawer';
   const {
     visible,
-    onChange,
+    onClose,
     closeable,
-    placement,
+    placement = 'top',
     container,
     width,
     zIndex,
     showMask,
     maskClosable,
     title,
+    footer,
+    headerStyle,
+    mainStyle,
+    footerStyle,
     children,
   } = props;
+  const componentRef = useRef<HTMLDivElement>(null);
+  const handleClose = () => {
+    onClose();
+  };
 
-  return <div>Drawer</div>;
+  useEffect(() => {
+    if (visible && showMask) document.body.style.overflow = 'hidden';
+    else document.body.style.overflow = 'auto';
+  }, [visible]);
+  const html = () => {
+    return (
+      <div
+        className={classnames(cn, cn + `-${placement}`, {
+          'g-drawer-open': visible,
+          'g-drawer-close': !visible,
+        })}
+        ref={componentRef}
+      >
+        {!!showMask && (
+          <div
+            className={cn + '-mask'}
+            onClick={maskClosable ? handleClose : undefined}
+          />
+        )}
+        <div
+          className={classnames(cn + '-content')}
+          style={{
+            zIndex,
+            width,
+          }}
+        >
+          <div className={cn + '-header'} style={headerStyle}>
+            {title}
+            {closeable && (
+              <CloseCircleOutlined
+                className="close-button"
+                onClick={handleClose}
+              />
+            )}
+          </div>
+          <div className={cn + '-main'} style={mainStyle}>
+            {children}
+          </div>
+          {footer && (
+            <div className={cn + '-footer'} style-={footerStyle}>
+              {footer}
+            </div>
+          )}
+        </div>
+      </div>
+    );
+  };
+
+  return container === false
+    ? html()
+    : ReactDOM.createPortal(html(), document.body);
 };
 
 Drawer.defaultProps = {
--- src/Drawer/demos/basic.tsx
+++ src/Drawer/demos/basic.tsx
@@ -1,9 +1,61 @@
-import React from 'react';
+import React, { useState } from 'react';
 //@ts-ignore
-import { Drawer } from 'banana-ui';
+import { Button, Drawer, RadioGroup, Radio } from 'banana-ui';
 
 const Demo = () => {
-  return <Drawer />;
+  const [visible, setVisible] = useState(false);
+  const [x, setX] = useState('right');
+  const handleClose = () => {
+    setVisible(false);
+  };
+  const handleChange = (value: string) => {
+    setX(value);
+  };
+  return (
+    <>
+      <RadioGroup value={x} onChange={handleChange}>
+        <Radio value={'left'}>left</Radio>
+        <Radio value={'right'}>right</Radio>
+      </RadioGroup>
+      <br />
+      <Button onClick={() => setVisible(true)}>show</Button>
+      <Drawer
+        placement={x}
+        visible={visible}
+        onClose={handleClose}
+        title="这是一个Drawer"
+        footer="footer"
+      >
+        <p>title</p>
+        <p>title</p>
+        <p>title</p>
+        <p>title</p>
+        <p>title</p>
+        <p>title</p>
+        <p>title</p>
+        <p>title</p>
+        <p>title</p>
+        <p>title</p>
+        <p>title</p>
+        <p>title</p>
+        <p>title</p>
+        <p>title</p>
+        <p>title</p>
+        <p>title</p>
+        <p>title</p>
+        <p>title</p>
+        <p>title</p>
+        <p>title</p>
+        <p>title</p>
+        <p>title</p>
+        <p>title</p>
+        <p>title</p>
+        <p>title</p>
+        <p>title</p>
+        <p>title</p>
+      </Drawer>
+    </>
+  );
 };
 
 export default Demo;

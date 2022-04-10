4b8270b5010e668dfdbcf4f5741c8297cd3e7c7c
Gy <406631892@qq.com>
2022-04-10 23:34:04 +0800

    修正CollapseItem

--- src/Collapse/CollapseItem.tsx
+++ src/Collapse/CollapseItem.tsx
@@ -1,27 +1,59 @@
-import React, { useEffect, useState } from 'react';
-import { CollapseItemProps } from '@/Collapse/types';
+import React, { useMemo } from 'react';
+import { CollapseItemProps } from './types';
 import classnames from 'classnames';
+import { Transition } from 'react-transition-group';
 
 const CollapseItem: React.FC<CollapseItemProps> = (props) => {
   const { title, children, selected, change } = props;
-  const [open, setOpen] = useState<boolean>(false);
   const handleClick = () => {
     change(props.name);
   };
-  useEffect(() => {
-    if (selected?.includes(props.name)) setOpen(true);
-    else setOpen(false);
-  }, [selected]);
+  const open = useMemo(() => selected.indexOf(props.name) >= 0, [selected]);
+
+  const onEntering = (node: HTMLElement) => {
+    let { height } = node.getBoundingClientRect();
+    node.style.height = 0 + 'px';
+    node.getBoundingClientRect();
+    node.style.height = `${height}px`;
+  };
+  const onEntered = (node: HTMLElement) => {
+    node.style.height = 'auto';
+  };
+  const onExiting = (node: HTMLElement) => {
+    const { height } = node.getBoundingClientRect();
+    node.style.height = height + 'px';
+    node.getBoundingClientRect();
+    node.style.height = '0px';
+  };
+  const onExited = (node: HTMLElement) => {
+    node.style.height = 'auto';
+    node.classList.add('gone');
+  };
+  const addEndListener = (node: HTMLElement, done: any) => {
+    // use the css transitionend event to mark the finish of a transition
+    node.addEventListener('transitionend', done, false);
+  };
   return (
     <div className="g-collapse-item">
       <div className="g-collapse-item-title" onClick={handleClick}>
         {title}
       </div>
-      {open && (
-        <div className={classnames('g-collapse-item-content', { open })}>
+      <Transition
+        in={open}
+        timeout={250}
+        unmountOnExit
+        onEntering={onEntering}
+        onEntered={onEntered}
+        onExiting={onExiting}
+        onExited={onExited}
+        addEndListener={addEndListener}
+      >
+        <div
+          className={classnames('g-collapse-item-content', { close: !open })}
+        >
           {children}
         </div>
-      )}
+      </Transition>
     </div>
   );
 };

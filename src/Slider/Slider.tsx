47fa1c7538958f67b066bf2a13bad85bbd21c258
Gy <406631892@qq.com>
2022-04-17 21:13:55 +0800

    Slider支持滑动事件

--- src/Slider/Slider.tsx
+++ src/Slider/Slider.tsx
@@ -1,4 +1,10 @@
-import React, { FC, MouseEvent, useMemo, useRef } from 'react';
+import React, {
+  FC,
+  useMemo,
+  useRef,
+  useEffect,
+  TouchEventHandler,
+} from 'react';
 import './Slider.scss';
 
 interface Props {
@@ -13,13 +19,13 @@ interface Props {
 const Slider: FC<Props> = (props) => {
   const { value, onChange, min = 0, max = 100 } = props;
   const sliderRef = useRef<HTMLDivElement>(null);
+  const draggingRef = useRef<boolean>(false);
   const percent = useMemo(() => {
     if (value < min || value > max)
       throw new Error('The value must between min and max');
     return Math.round(((value - min) / (max - min)) * 100);
   }, [value, min, max]);
-  console.log(percent);
-  const handleClick = (e: MouseEvent) => {
+  const handleClick = (e: React.MouseEvent) => {
     const { left, width } = sliderRef.current!.getBoundingClientRect();
     if (e.clientX >= left && e.clientX <= left + width) {
       const percent = Math.round(((e.clientX - left) / width) * 100) / 100;
@@ -27,11 +33,66 @@ const Slider: FC<Props> = (props) => {
       onChange(value);
     }
   };
+  const handleMouseUp = () => {
+    draggingRef.current = false;
+  };
+  const handleMouseMove = (e: MouseEvent) => {
+    if (draggingRef.current === false) return;
+    const { left, width } = sliderRef.current!.getBoundingClientRect();
+    if (e.clientX >= left && e.clientX <= left + width) {
+      const percent = Math.round(((e.clientX - left) / width) * 100) / 100;
+      const value = Math.round((max - min) * percent + min);
+      onChange(value);
+    }
+  };
+  const handleMouseDown = () => {
+    draggingRef.current = true;
+  };
+  const handleSelectStart = (e: Event) => {
+    if (draggingRef.current) e.preventDefault();
+  };
+  const handleTouchStart = () => {
+    draggingRef.current = true;
+  };
+  const handleTouchMove: TouchEventHandler<HTMLDivElement> = (e) => {
+    const { left, width } = sliderRef.current!.getBoundingClientRect();
+    if (e.touches[0].clientX >= left && e.touches[0].clientX <= left + width) {
+      const percent =
+        Math.round(((e.touches[0].clientX - left) / width) * 100) / 100;
+      const value = Math.round((max - min) * percent + min);
+      onChange(value);
+    }
+  };
+  const handleTouchEnd = () => {
+    draggingRef.current = false;
+  };
+  useEffect(() => {
+    document.addEventListener('mouseup', handleMouseUp);
+    document.addEventListener('mousemove', handleMouseMove);
+    document.addEventListener('selectstart', handleSelectStart);
+    return () => {
+      document.removeEventListener('mouseup', handleMouseUp);
+      document.removeEventListener('mousemove', handleMouseMove);
+      document.addEventListener('selectstart', handleSelectStart);
+    };
+  }, []);
+
   return (
-    <div className="g-slider" onClick={handleClick} ref={sliderRef}>
+    <div
+      className="g-slider"
+      onClick={handleClick}
+      ref={sliderRef}
+      onTouchStart={handleTouchStart}
+      onTouchMove={handleTouchMove}
+      onTouchEnd={handleTouchEnd}
+    >
       <div className="g-slider-rail" />
       <div className="g-slider-track" style={{ width: percent + '%' }} />
-      <div className="g-slider-handle" style={{ left: percent + '%' }} />
+      <div
+        className="g-slider-handle"
+        style={{ left: percent + '%' }}
+        onMouseDown={handleMouseDown}
+      />
     </div>
   );
 };
--- src/Slider/demos/basic.tsx
+++ src/Slider/demos/basic.tsx
@@ -1,12 +1,16 @@
 import React, { useState } from 'react';
 // @ts-ignore
 import { Slider } from 'banana-ui';
+import { useEffect } from 'react';
 
 export default function Demo() {
   const [value, setValue] = useState(190);
   const handleChange = (value: number) => {
     setValue(value);
   };
+  useEffect(() => {
+    console.log('你选中的value是:', value);
+  }, [value]);
   return (
     <>
       <Slider value={value} onChange={handleChange} min={100} max={250} />

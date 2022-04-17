af38b0e61773647da69cd2302fb0e80fc61033a5
Gy <406631892@qq.com>
2022-04-17 19:39:35 +0800

    Slider可以点击改变值

--- src/Slider/Slider.tsx
+++ src/Slider/Slider.tsx
@@ -1,10 +1,39 @@
-import React, { FC } from 'react';
+import React, { FC, MouseEvent, useMemo, useRef } from 'react';
 import './Slider.scss';
 
-interface Props {}
+interface Props {
+  value: number;
+  onChange: (value: number) => void;
+  min?: number;
+  max?: number;
+}
+
+// percent = (value - min) / (max - min)
 
 const Slider: FC<Props> = (props) => {
-  return <div>Slider</div>;
+  const { value, onChange, min = 0, max = 100 } = props;
+  const sliderRef = useRef<HTMLDivElement>(null);
+  const percent = useMemo(() => {
+    if (value < min || value > max)
+      throw new Error('The value must between min and max');
+    return Math.round(((value - min) / (max - min)) * 100);
+  }, [value, min, max]);
+  console.log(percent);
+  const handleClick = (e: MouseEvent) => {
+    const { left, width } = sliderRef.current!.getBoundingClientRect();
+    if (e.clientX >= left && e.clientX <= left + width) {
+      const percent = Math.round(((e.clientX - left) / width) * 100) / 100;
+      const value = Math.round((max - min) * percent + min);
+      onChange(value);
+    }
+  };
+  return (
+    <div className="g-slider" onClick={handleClick} ref={sliderRef}>
+      <div className="g-slider-rail" />
+      <div className="g-slider-track" style={{ width: percent + '%' }} />
+      <div className="g-slider-handle" style={{ left: percent + '%' }} />
+    </div>
+  );
 };
 
 export default Slider;
--- src/Slider/demos/basic.tsx
+++ src/Slider/demos/basic.tsx
@@ -1,5 +1,15 @@
-import React from 'react';
+import React, { useState } from 'react';
+// @ts-ignore
+import { Slider } from 'banana-ui';
 
 export default function Demo() {
-  return <div>basic</div>;
+  const [value, setValue] = useState(190);
+  const handleChange = (value: number) => {
+    setValue(value);
+  };
+  return (
+    <>
+      <Slider value={value} onChange={handleChange} min={100} max={250} />
+    </>
+  );
 }

ea9aac51f29d902190c466ccf00fbe429400609e
Gy <406631892@qq.com>
2022-05-02 23:39:22 +0800

    change test in drawer

--- src/Drawer/__test__/Drawer.test.tsx
+++ src/Drawer/__test__/Drawer.test.tsx
@@ -1,17 +1,18 @@
 import {
   act,
   render,
-  cleanup,
   screen,
   fireEvent,
   RenderResult,
   waitFor,
-  waitForElementToBeRemoved,
 } from '@testing-library/react';
 import '@testing-library/jest-dom';
 import React, { useState } from 'react';
 import Drawer, { DrawerProps } from '../Drawer';
 import Button from '../../Button/Button';
+import { config } from 'react-transition-group';
+
+config.disabled = true;
 
 let visible: boolean, setVisible: Function;
 const Demo = () => {
@@ -39,22 +40,37 @@ let wrapper: RenderResult, btn: HTMLButtonElement;
 
 describe('Testing Drawer component', () => {
   it('Drawer should be render correct', async () => {
-    wrapper = render(<Demo />);
+    act(() => {
+      wrapper = render(<Demo />, { container: document.body });
+    });
     btn = screen.getByText('show');
     expect(btn).toBeInTheDocument();
-    fireEvent.click(btn);
+    act(() => {
+      fireEvent.click(btn);
+    });
+    await waitFor(() => {
+      expect(wrapper.asFragment()).toMatchSnapshot();
+    });
     expect(visible).toBeTruthy();
     expect(screen.queryByText('title')).toBeInTheDocument();
-    expect(wrapper.asFragment()).toMatchSnapshot();
-    expect(
-      wrapper.container.querySelector('.g-drawer.g-drawer-right.g-drawer-open'),
-    ).toBeInTheDocument();
+    expect(document.querySelector('.g-drawer-open')).toBeInTheDocument();
     expect(wrapper.asFragment()).toMatchSnapshot();
   });
   it('should close', async () => {
-    wrapper = render(<Demo />);
+    wrapper = render(<Demo />, { container: document.body });
+    btn = screen.getByText('show');
+    act(() => {
+      fireEvent.click(btn);
+    });
     const closeBtn = document.getElementsByClassName('close-button')[0];
-    fireEvent.click(closeBtn);
-    await waitForElementToBeRemoved(() => screen.queryAllByText('title'));
+    act(() => {
+      fireEvent.click(closeBtn);
+    });
+    expect(
+      wrapper.container.querySelector(
+        '.g-drawer.g-drawer-right.g-drawer-close',
+      ),
+    ).toBeInTheDocument();
+    expect(wrapper.asFragment()).toMatchSnapshot();
   });
 });
